1904d703d5fd51ab98eb7c92ec49d16be531ac0a
Tony Seing <tony.seing@gmail.com>
2014-12-09 19:23:15 -0500

    adds code for on off switch

--- dog_casino.ino
+++ dog_casino.ino
@@ -5,12 +5,14 @@ int echo_pin = 7;
 int trig_pin = 8;
 int servo_pin = 9;
 int servo_angle = 0;
+const int power_button_pin = 4;     
 
 int sensorValue = 1000;  
-int triggerBoundary = 18;   // you should calibrate--this is the distance boundary of a physical object from the sensor to trigger some action
+int triggerBoundary = 19;   // you should calibrate--this is the distance boundary of a physical object from the sensor to trigger some action
 
 Servo servo;  
 
+boolean power_state = false; // power off until button pressed
 
 void setup() {
   
@@ -30,41 +32,63 @@ void feedDog(int amount) {
   for(servo_angle = 0; servo_angle < 180; servo_angle++)  
   {                                  
     servo.write(servo_angle);               
-    delay(5);                   
+    delay(2);                   
   } 
   
   // now turn back 180 degrees to 0 degrees
   for(servo_angle = 180; servo_angle > 0; servo_angle--)    
   {                                
     servo.write(servo_angle);           
-    delay(5);       
+    delay(2);       
   } 
 }
 
+void swap_power_state() {
+  if (power_state == true)
+  {
+    power_state = false;
+  } else {
+    power_state = true;
+  }
+}
 
 void loop() {
-  long duration, distance;
-  
-  // send the pulse
-  digitalWrite(trig_pin, LOW); 
-  delayMicroseconds(2); // low for 2 microseconds
-  digitalWrite(trig_pin, HIGH);
-  delayMicroseconds(10); // high for 10 microseconds
-  digitalWrite(trig_pin, LOW);
-  duration = pulseIn(echo_pin, HIGH); // measure the time to the echo
-  distance = (duration / 2) / 29.1;  // calculate the distance in centimeters
+  Serial.println("-------------------");
+  Serial.println(digitalRead(power_button_pin));
+  if (digitalRead(power_button_pin) == LOW)
+  {
+    swap_power_state();
+    Serial.println("Power state changed");
+    Serial.println(power_state);
+  }
   
    
-  if (distance < triggerBoundary) {
-    Serial.println("DOG DETECTED");  
-    Serial.println(distance);
-    feedDog(1);
-  }
-  else {
-    Serial.println("NO DOG DETECTED");
-    Serial.println(distance);
-  }
+  if (power_state == true) {
+    
+    long duration, distance;
   
+    // send the pulse
+    digitalWrite(trig_pin, LOW); 
+    delayMicroseconds(2); // low for 2 microseconds
+    digitalWrite(trig_pin, HIGH);
+    delayMicroseconds(10); // high for 10 microseconds
+    digitalWrite(trig_pin, LOW);
+    duration = pulseIn(echo_pin, HIGH); // measure the time to the echo
+    distance = (duration / 2) / 29.1;  // calculate the distance in centimeters
+    
+     
+    if (distance < triggerBoundary) {
+      Serial.println("DOG DETECTED");  
+      Serial.println(distance);
+      feedDog(1);
+    }
+    else {
+      Serial.println("NO DOG DETECTED");
+      Serial.println(distance);
+    }
+    
+     delay(150);   
+  }
  
-  delay(200);         
+  delay(100);      
 }
